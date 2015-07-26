7d28ec31ea86a847581a92d87b7f90e30eae1470
teraharas <triple.zero.option@gmail.com>
2015-07-26 10:12:49 +0000

    Add age

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -43,7 +43,7 @@ class MessagesController < ApplicationController
   def message_params
     # params[:message]のパラメータで name , bodyのみを許可する。
     # 返り値は ex:) {name: "入力されたname" , body: "入力されたbody" }
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :age, :body)
   end
   
   def set_message
--- app/models/message.rb
+++ app/models/message.rb
@@ -1,6 +1,8 @@
 class Message < ActiveRecord::Base
     # 名前は必須、20文字以内
     validates :name, length: { maximum: 20 }, presence: true
+    # 年齢は数字のみ
+    validates :age, length: { maximum: 3 }, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, presence: true
     # 内容は必須、2～30字
     validates :body, length: { minimum: 2, maxmum: 30 }, presence: true
 end
