2639693c9b0e4b00eb7b6cadc88bb9511e3e8d38
teraharas <triple.zero.option@gmail.com>
2015-07-26 07:25:29 +0000

    Implementation of the validation

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ActiveRecord::Base
+    # 名前は必須、20文字以内
+    validates :name, length: { maximum: 20 }, presence: true
+    # 内容は必須、2～30字
+    validates :body, length: { minimum: 2, maxmum: 30 }, presence: true
 end
